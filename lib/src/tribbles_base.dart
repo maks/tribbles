adc5531ae3068da144043fbf44288fb8af15ed41
Maksim Lin <maks@manichord.com>
2021-02-02 12:35:07 +1100

    add actual Isoaltes to tribbles

--- lib/src/tribbles_base.dart
+++ lib/src/tribbles_base.dart
@@ -1,26 +1,39 @@
+import 'dart:isolate';
+
 /// Each Tribble represents a single Isolate.
 class Tribble {
   static final List<Tribble> _tribbles = [];
 
-  bool alive = true;
+  bool get alive => _isolate != null;
+
+  Isolate? _isolate;
+  final ReceivePort _responses = ReceivePort();
 
   /// Total number of currently running tribbles
   static int get count => _tribbles.length;
 
   /// Create a new tribble
   Tribble() {
+    Isolate.spawn(_isolateHandler, _responses.sendPort)
+        .then((i) => _isolate = i);
     _tribbles.add(this);
   }
 
   void kill() {
-    alive = false;
+    _isolate?.kill();
+    _isolate = null;
     _tribbles.remove(this);
   }
 
   static void killAll() {
-    for (final t in _tribbles) {
-      t.alive = false;
-    }
+    final old = List.from(_tribbles);
     _tribbles.clear();
+    for (final t in old) {
+      t._isolate?.kill();
+    }
   }
+}
+
+void _isolateHandler(SendPort responsePort) {
+  //TODO: use responsePort to send requestPort
 }
--- test/tribbles_test.dart
+++ test/tribbles_test.dart
@@ -1,8 +1,9 @@
+import 'package:tribbles/src/tribbles_base.dart';
 import 'package:tribbles/tribbles.dart';
 import 'package:test/test.dart';
 
 void main() {
-  setUp(() {
+  setUp(() async {
     Tribble.killAll();
   });
 
@@ -10,7 +11,6 @@ void main() {
     test('creating tribble increases tribble count', () {
       // ignore: unused_local_variable
       final tribble = Tribble();
-
       expect(Tribble.count, equals(1));
     });
 
@@ -23,8 +23,16 @@ void main() {
       expect(Tribble.count, equals(0));
     });
 
-    test('killing a tribble marks it as an not alive', () {
+    test('killing a tribble marks it as no longer alive', () async {
       final tribble = Tribble();
+
+      // yuck! but we want to test actual creation of isolates
+      // so allow for upto 250ms for a isoolate to be created
+      var count = 0;
+      while (!tribble.alive && count < 10) {
+        await Future.delayed(Duration(milliseconds: 25));
+        count++;
+      }
       expect(tribble.alive, isTrue);
 
       tribble.kill();
